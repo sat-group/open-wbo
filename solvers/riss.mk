a4e5ea91ed8a69d281af718a74e7bb34fda7d970
Norbert Manthey <nmanthey@conp-solutions.com>
2018-05-12 23:03:19 +0200

    riss: enable reserveSATVariable method
    By adding the SAT_HAS_RESERVATION compile flag, Riss now uses the
    reserveSATVariable method.

--- solvers/riss.mk
+++ solvers/riss.mk
@@ -9,3 +9,6 @@ MROOT      = $(PWD)/solvers/$(SOLVERDIR)/riss
 CFLAGS     += -I .. -I ../.. -I ../../.. -I solvers/riss -I ../coprocessor -g3
 CFLAGS     += -Wno-delete-non-virtual-dtor -Wno-unused-variable -Wno-sign-compare -Wno-parentheses -Wno-unused-but-set-variable -Wno-reorder
 LFLAGS     += -lpthread -lz -lrt
+
+# Riss has the ability to reserve variables in a range
+CFLAGS     += -DSAT_HAS_RESERVATION
